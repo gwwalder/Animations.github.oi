4fce5955ba45dbca7c7faac8a12fd51ef774d0d4
gwwalder <gwwalder@gmail.com>
2018-10-06 00:24:16 -0700

    update
    manual c/p

--- js/animation.js
+++ js/animation.js
@@ -1,32 +1,27 @@
-
 $('#click').on('click',
 function fadeOut() {
   TweenMax.to(".btn", 0.1, {
     y: -100,
     opacity: 0
   });
-
-  TweenMax.to(".screen", 0.5, {
-    y: -400,
-    opacity: 0,
-    ease: Power2.easeInOut,
-  });
-
   TweenMax.from(".overlay", 0.5, {
-    ease: Power2.easeInOut
+    ease: Power2.easeInOut,
+    overflow:'hidden'
   });
   TweenMax.to(".overlay", 1, {
     top: "-120%",
-    ease: Expo.easeInOut
+    ease: Expo.easeInOut,
+    overflow:'hidden'
   })
 TweenMax.to(".overlay2", 2, {
   top: "-120%",
-  ease: Expo.easeInOut
+  ease: Expo.easeInOut,
+  overflow:'hidden',
 });
-TweenMax.to(".testHeader", 4, {
-  delay: 0.8,
-  opacity: 1,
-  y: -150,
-  ease: Power2.easeInOut
+TweenMax.from(".testHeader", 3, {
+  delay: 0.5,
+  top: "100%",
+  ease: Power2.easeInOut,
+  overflow:'hidden'
 });
 });
