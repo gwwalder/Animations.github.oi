67303eda7426fb5788b5de5c5e540e84f3cbb6d7
George Walder <gwwalder@gmail.com>
2018-10-06 22:29:56 -0700

    removed bg img

--- js/animation.js
+++ js/animation.js
@@ -1,27 +1,27 @@
 $('#click').on('click',
-function fadeOut() {
-  TweenMax.to(".btn", 0.1, {
-    y: -100,
-    opacity: 0
+  function fadeOut() {
+    TweenMax.to(".btn", 0.1, {
+      y: -100,
+      opacity: 0
+    });
+    TweenMax.from(".overlay", 0.5, {
+      ease: Power2.easeInOut,
+      overflow: 'hidden'
+    });
+    TweenMax.to(".overlay", 1, {
+      top: "-120%",
+      ease: Expo.easeInOut,
+      overflow: 'hidden'
+    })
+    TweenMax.to(".overlay2", 2, {
+      top: "-120%",
+      ease: Expo.easeInOut,
+      overflow: 'hidden',
+    });
+    TweenMax.from(".testHeader", 3, {
+      delay: 0.5,
+      top: "100%",
+      ease: Power2.easeInOut,
+      overflow: 'hidden'
+    });
   });
-  TweenMax.from(".overlay", 0.5, {
-    ease: Power2.easeInOut,
-    overflow:'hidden'
-  });
-  TweenMax.to(".overlay", 1, {
-    top: "-120%",
-    ease: Expo.easeInOut,
-    overflow:'hidden'
-  })
-TweenMax.to(".overlay2", 2, {
-  top: "-120%",
-  ease: Expo.easeInOut,
-  overflow:'hidden',
-});
-TweenMax.from(".testHeader", 3, {
-  delay: 0.5,
-  top: "100%",
-  ease: Power2.easeInOut,
-  overflow:'hidden'
-});
-});
