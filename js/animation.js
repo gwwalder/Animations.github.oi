4426489c14cbc529f4561cec8901ddfb831fad67
gww <gwwalder@gmail.com>
2018-10-24 14:51:31 -0700

    Finished Resume page

--- js/animation.js
+++ js/animation.js
@@ -3,6 +3,7 @@ window.onbeforeunload = function() {
   window.scrollTo(0, 0);
 };
 $(document).ready(function(){
+
 $("body").css("overflow", "hidden");
 // animates css nav bar transisions
 $('.menuToggle').click(function() {
